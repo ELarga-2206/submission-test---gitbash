8c1db6c02261bf74d4266d759e514db60ae2b476
ELarga-2206 <earl.larga22@gmail.com>
2025-02-26 12:11:42 +0800

    hello world

---
+++ main_uploadtest.py
@@ -0,0 +1,12 @@
+#lets test the code
+#hello there
+
+user = input("is this working?")
+if user == "yes":
+    print("thats great!")
+if user == "no":
+    print("skill issue bro")
+else:
+    print("i dunno bro lmao")
+
+    
